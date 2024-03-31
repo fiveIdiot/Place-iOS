e5c08a6f739e7d889ea00a72a15142b6822c1e0b
minaje0917 <k050918@naver.com>
2024-03-31 14:31:36 +0900

    :sparkles: Add :: BaseViewController, BaseViewModel

---
+++ Projects/Features/BaseFeature/Sources/Base/BaseViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewModel.swift
+//  BaseFeature
+//
+//  Created by 선민재 on 3/31/24.
+//  Copyright © 2024 fiveIdiot. All rights reserved.
+//
+
+protocol BaseViewModel {
+    associatedtype Input
+    associatedtype Output
+        
+    func transform(input: Input) -> Output
+}
