6013c51c438346458fc2d231dd3cf9fac240870c
minaje0917 <k050918@naver.com>
2024-04-01 22:01:27 +0900

    :sparkles: Feature: Map, WritePlace, Store, Profile Feature Setting

---
+++ Projects/Features/WritePlace/WritePlaceFeature/Sources/Scene/WritePlaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WritePlaceViewController.swift
+//  WritePlaceFeature
+//
+//  Created by 선민재 on 4/1/24.
+//  Copyright © 2024 fiveIdiot. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+import BaseFeature
+
+final class WritePlaceViewController: BaseViewController {
+    
+}
---
+++ Projects/Features/WritePlace/WritePlaceFeature/Sources/Scene/WritePlaceViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  WritePlaceViewModel.swift
+//  WritePlaceFeature
+//
+//  Created by 선민재 on 4/1/24.
+//  Copyright © 2024 fiveIdiot. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+import BaseFeature
+
+final class WritePlaceViewModel: BaseViewModel {
+    struct Input {}
+    
+    struct Output {}
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
